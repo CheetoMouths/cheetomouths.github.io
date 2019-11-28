6e5458fbbc67255023a9110e8acabebc85bc32c6
CheetoMouths <58275352+CheetoMouths@users.noreply.github.com>
2019-11-27 17:55:28 -0800

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -6,3 +6,29 @@ import Navbar from './navbar.vue';
 Vue.component(Editor.name, Editor);
 Vue.component(Loader.name, Loader);
 Vue.component(Navbar.name, Navbar);
+
+/// get context for canvas, cache dimension
+var ctx = demo.getContext('2d'),
+    w = demo.width,
+    h = demo.height,
+
+    img = favicon.ico(); /// the image we want to load
+
+/// when done go draw existing marks and start listening for clicks
+img.onload = function() {
+
+    renderMarks();
+
+    demo.onclick = function(e) {
+
+        /// convert mouse coord relative to canvas
+        var rect = demo.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+
+        /// store mark
+        addMark(x, y);
+
+        /// redraw everything
+        renderMarks();
+    }
