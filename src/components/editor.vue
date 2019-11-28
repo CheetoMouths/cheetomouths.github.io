65993ff4e41b108b7a7c83795341164bf2424c29
CheetoMouths <58275352+CheetoMouths@users.noreply.github.com>
2019-11-27 17:57:50 -0800

    Update editor.vue

--- src/components/editor.vue
+++ src/components/editor.vue
@@ -24,7 +24,7 @@
       >
       <button
         class="toolbar__button"
-        data-action="move"
+        data-action="cheeto"
         title="Add Cheeto Dust"
       >
         <span class="fa fa-arrows" />
@@ -128,6 +128,33 @@ export default {
           cropper.zoom(0.1);
           break;
 
+        case 'cheeto'
+          /// get context for canvas, cache dimension
+    var ctx = demo.getContext('2d'),
+    w = demo.width,
+    h = demo.height,
+
+    img = favicon.ico(); /// the image we want to load
+
+/// when done go draw existing marks and start listening for clicks
+img.onload = function() {
+
+    renderMarks();
+
+    demo.onclick = function(e) {
+
+        /// convert mouse coord relative to canvas
+        var rect = demo.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+
+        /// store mark
+        addMark(x, y);
+
+        /// redraw everything
+        renderMarks();
+    }
+          
         case 'zoom-out':
           cropper.zoom(-0.1);
           break;
